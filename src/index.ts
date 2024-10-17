f498cfff9e9e0bf5fe987ab3907a34f9170f6836
Felipe Conrado <fconrado2003@gmail.com>
2024-10-17 15:49:24 -0300

    :wrench: package config

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,25 @@
 type Task = {
-    name: string,
-    schedule: Date
-}
+  name: string;
+  schedule: Date;
+};
 
-class ToDoListManager{
-    private tasks: Task[] = []
-    constructor() {}
+class ToDoListManager {
+  private tasks: Task[] = [];
+  constructor() {}
 
-    insertTask(task: any){
-        this.tasks.push(task)
-    }
-
+  insertTask(task: any) {
+    this.tasks.push(task);
+  }
 }
 
 const task: Task = {
+  name: "aula 03",
 
-    name: 'aula 03',
-    
-    schedule: new Date
-    ('2004-08-23')
-}
+  schedule: new Date("2004-08-23"),
+};
 
-const taskManager = new 
-ToDoListManager()
+const taskManager = new ToDoListManager();
 
-taskManager.insertTask(task)
+taskManager.insertTask(task);
+
+console.log('TASK', task)
