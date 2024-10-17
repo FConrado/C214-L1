7ec6a9e81cf6d75e1b5ec4336d05a91a5f4f1b57
Felipe Conrado <fconrado2003@gmail.com>
2024-10-17 15:40:20 -0300

    :wrench: config husky and prettier

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+type Task = {
+    name: string,
+    schedule: Date
+}
+
+class ToDoListManager{
+    private tasks: Task[] = []
+    constructor() {}
+
+    insertTask(task: any){
+        this.tasks.push(task)
+    }
+
+}
+
+const task: Task = {
+
+    name: 'aula 03',
+    
+    schedule: new Date
+    ('2004-08-23')
+}
+
+const taskManager = new 
+ToDoListManager()
+
+taskManager.insertTask(task)
